06f32128462899ac87ad1d47f24e02d3c2ae006d
Andres Felipe Arboleda Correa <afarboledac@unal.edu.co>
2022-07-04 01:06:31 -0500

    se añade test en compute_daily_prices

--- src/data/compute_daily_prices.py
+++ src/data/compute_daily_prices.py
@@ -16,9 +16,18 @@ def compute_daily_prices():
     * fecha: fecha en formato YYYY-MM-DD
 
     * precio: precio promedio diario de la electricidad en la bolsa nacional
-
-
-
+    >>> compute_daily_prices()
+           Fecha    Precio
+    0 1995-07-20  1.409435
+    1 1995-07-21  4.924333
+    2 1995-07-22  1.269500
+    3 1995-07-23  0.953083
+    4 1995-07-24  4.305917
+    5 1995-07-25  1.149167
+    6 1995-07-26  1.108625
+    7 1995-07-27  0.585958
+    8 1995-07-28  0.499792
+    9 1995-07-29  0.927667
     """
     module_path = os.path.dirname(__file__)
     folder_path = os.path.join(
@@ -38,6 +47,7 @@ def compute_daily_prices():
     precios_diarios = precios_horarios[["Fecha", "Precio"]]
 
     precios_diarios.to_csv(target_folder, index=False)
+    print(precios_diarios.head(10))
 
 
 if __name__ == "__main__":
