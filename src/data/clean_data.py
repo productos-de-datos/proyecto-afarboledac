addb32396614b4cfca02faf66e42eb89e4cdef7e
Andres Felipe Arboleda Correa <afarboledac@unal.edu.co>
2022-07-04 11:32:28 -0500

    se añade test de clean data se corrige test computemonthly

--- src/data/clean_data.py
+++ src/data/clean_data.py
@@ -98,10 +98,14 @@ def clean_data():
     * fecha: fecha en formato YYYY-MM-DD
     * hora: hora en formato HH
     * precio: precio de la electricidad en la bolsa nacional
-
     Este archivo contiene toda la información del 1997 a 2021.
-
-
+    >>> clean_data()
+           Fecha      0      1      2      3      4      5      6      7      8      9     10     11     12     13     14     15     16     17      18      19      20     21     22     23
+    0 1995-07-20    NaN  1.073  1.073  1.073  1.073  1.073  1.073  1.073  1.074  1.074  2.827  2.827  2.827  1.074  1.073  1.073  1.073  1.073   1.074   1.897   1.897  1.897  1.073  1.073
+    1 1995-07-21  1.073  1.000  1.000  1.000  1.000  1.000  5.000  6.000  6.000  6.000  6.000  9.256  9.256  5.000  5.000  1.000  1.000  5.000  12.000  16.670  11.929  5.000  1.000  1.000
+    2 1995-07-22  1.073  1.073  1.000  1.000  1.000  1.073  1.303  1.303  1.303  1.303  1.303  1.303  1.303  1.303  1.073  1.000  1.000  1.000   1.303   2.500   2.500  1.303  1.073  1.073
+    3 1995-07-23  1.073  1.000  1.000  1.000  1.000  1.000  0.100  1.000  1.000  1.000  1.000  1.000  1.238  1.238  1.000  0.100  0.100  1.000   1.238   1.238   1.238  1.238  1.073  1.000
+    4 1995-07-24  1.000  1.000  0.990  1.000  1.000  1.073  3.000  3.000  3.000  3.500  8.845  9.256  3.000  1.073  1.073  1.073  3.000  2.000  18.630  22.500   9.256  3.000  1.073  1.000
     """
     module_path = os.path.dirname(__file__)
     folder_path = os.path.join(module_path, "../../data_lake/raw/*")
@@ -113,6 +117,7 @@ def clean_data():
     resultado = consolidar_archivos(files)
     resultado = formatear_archivo(resultado)
     resultado = resultado.drop_duplicates()
+    print(resultado.head())
     resultado.to_csv(target_path, index=False)
 
 
